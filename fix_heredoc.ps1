0ad98757771e2c109b32a1e918205c358a918e57
whatuneeed <nsskurat@gmail.com>
2026-04-02 13:50:19 +0400

    player.html: синхронизация с v3.9 (PiP, избранное), удаление fix_heredoc.ps1

--- fix_heredoc.ps1
+++
@@ -1,5 +0,0 @@
-$lines = Get-Content 'C:\IPTV-Manager\IPTV-Manager.sh'
-for ($i = 225; $i -le 428; $i++) {
-    $lines[$i] = $lines[$i].Replace('\$', '$').Replace('\\\\', '\\').Replace('%%', '%')
-}
-Set-Content 'C:\IPTV-Manager\IPTV-Manager.sh' $lines -NoNewline
